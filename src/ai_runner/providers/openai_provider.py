d4cd5c3550bfed4ecd91f42a1c1feba791c0e473
supar <omsupra89@gmail.com>
2026-07-12 16:56:07 +0700

    Update openai_provider.py

--- src/ai_runner/providers/openai_provider.py
+++ src/ai_runner/providers/openai_provider.py
@@ -1,1 +1,20 @@
+from openai import OpenAI
+import os
 
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+
+def ask(prompt):
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
